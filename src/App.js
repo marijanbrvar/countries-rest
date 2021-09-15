628bd85e2ac01a4f3f380bc0cd1ee0cbdb66eeef
Marijan Brvar <marijan.brvar@gmail.com>
2021-09-15 10:51:08 +0300

    Add fetch feature for all european countries

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import './App.css';
+/* eslint-disable react/jsx-key */
+/* eslint-disable no-use-before-define */
+/* eslint-disable react/prefer-stateless-function */
+import React, { useState, useEffect } from 'react';
+import Navbar from './componetns/layouts/Navbar';
 
-function App() {
+const App = () => {
+  const [countires, setCountires] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getCountires();
+  }, []);
+
+  const getCountires = async () => {
+    setLoading(true);
+    const res = await fetch('https://restcountries.com/v2/continent/europe');
+    const data = await res.json();
+    setCountires(data);
+    setLoading(false);
+  };
+
+  if (loading) return <h4>Loading...</h4>;
+
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div>
+      <Navbar />
+      <h1>Main page</h1>
+      {!loading && countires.length === 0 ? (<p>No countries to show</p>) : (
+        countires.map((country) => (
+          <li key={country.alpha2Code}>
+            {country.name}
+            {' '}
+            /
+            {country.population}
+            {country.nativeName}
+          </li>
+        ))
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/componetns/layouts/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Navbar = () => (
+  <nav className="navbar bg-primary">
+    <h1>Header</h1>
+  </nav>
+);
+
+export default Navbar;

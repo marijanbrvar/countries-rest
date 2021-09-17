4b6814459d88da4c7ca15442b89c0f72067d26a4
Marijan Brvar <marijan.brvar@gmail.com>
2021-09-18 00:20:51 +0300

    Fix eslint offenses

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,4 @@
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import App from './App';
 import reducer from './store/reducers/countriesReducer';
 import { getCountries } from './store/__mocks__/api';

5915fb90289e0a82dce67d4330f0095038b8a0da
Marijan Brvar <marijan.brvar@gmail.com>
2021-09-18 00:19:56 +0300

    Add tests for store and for nav rendering

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,39 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
+import reducer from './store/reducers/countriesReducer';
+import { getCountries } from './store/__mocks__/api';
+import countries from './store/__mocks__/countries';
 
-test('renders learn react link', () => {
+test('Renders Countries react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/European/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('Return initial state', () => {
+  const initialState = {
+    countries: [],
+    current: null,
+    error: null,
+    loading: false,
+  };
+  const stateNew = reducer(undefined, {});
+  expect(stateNew).toEqual(initialState);
+});
+
+test('Returns received state', () => {
+  const initialState = {
+    countries,
+    current: null,
+    error: null,
+    loading: false,
+  };
+  const stateNew = reducer(initialState, getCountries(countries));
+  expect(stateNew).toEqual({
+    countries,
+    current: null,
+    error: null,
+    loading: false,
+  });
 });

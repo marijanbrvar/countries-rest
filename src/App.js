b1b2a0fbe6aa987a9686f6e4aa068351aec7e877
Marijan Brvar <marijan.brvar@gmail.com>
2021-09-18 01:14:23 +0300

    Fix navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -9,17 +9,19 @@ import CountryInfo from './componetns/pages/CountryInfo';
 import Countries from './componetns/pages/Countries';
 
 const App = () => (
-  <Provider store={store}>
-    <Navbar />
-    <Switch>
-      <Route
-        exact
-        path="/"
-        component={Countries}
-      />
-      <Route path="/:code" component={CountryInfo} />
-    </Switch>
-  </Provider>
+  <Router>
+    <Provider store={store}>
+      <Navbar />
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={Countries}
+        />
+        <Route path="/:code" component={CountryInfo} />
+      </Switch>
+    </Provider>
+  </Router>
 );
 
 export default App;
--- src/componetns/layouts/Navbar.js
+++ src/componetns/layouts/Navbar.js
@@ -1,34 +1,25 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
-  const history = useHistory();
-
-  return (
-    <nav>
-      <div className="navbar">
-        <Link
-          to="/"
-          className="brand-logo left"
-        >
-          {history.location.pathname !== '/' ? (
-            <div>
-              <i className="material-icons">chevron_left</i>
-            </div>
-          ) : (
-            <div>
-              European Countries
-            </div>
-          )}
-        </Link>
-        <ul className="right">
-          <li><i className="material-icons mx-1">mic</i></li>
-          <li><i className="material-icons">settings</i></li>
-        </ul>
-      </div>
-    </nav>
-  );
-};
+const Navbar = () => (
+  <nav>
+    <div className="navbar">
+      <Link
+        to="/"
+        className="brand-logo left"
+      >
+        <div className="logo">
+          <i className="material-icons">chevron_left</i>
+          <h5>European Countries</h5>
+        </div>
+      </Link>
+      <ul className="right">
+        <li><i className="material-icons mx-1">mic</i></li>
+        <li><i className="material-icons">settings</i></li>
+      </ul>
+    </div>
+  </nav>
+);
 
 export default Navbar;
